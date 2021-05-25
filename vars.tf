75829bea7489acdb7dea6a912141cfda756bc891
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-25 09:03:57 +0530

    roboshop

--- vars.tf
+++ vars.tf
@@ -8,8 +8,6 @@ variable "DOMAIN" {default = ""}
 
 variable "COMPONENT" {}
 variable "AMI" {}
-variable "PASSWORD" {}
-variable "USER" {}
 variable "ERLANG" {
     default = "http://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_23.2.3-1~centos~7_amd64.rpm"
 }
