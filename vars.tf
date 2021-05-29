a8a581519612c3bc060e4d9b53412032abc5dc83
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-29 12:52:04 +0530

    rs-webapp

--- vars.tf
+++ vars.tf
@@ -7,6 +7,8 @@ variable "R53_ZONE_ID" {}
 variable "DOMAIN" {default = ""}
 
 variable "COMPONENT" {}
+
+variable "env" {}
 variable "ERLANG" {
     default = "http://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_23.2.3-1~centos~7_amd64.rpm"
 }
